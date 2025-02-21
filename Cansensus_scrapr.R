25f223b3168635c50552211efd2f91bb0ee9023e
Andrew Girgis <Andrew_Girgis@Hotmail.com>
2025-02-21 14:01:52 -0500

    updated code

--- Cansensus_scrapr.R
+++ Cansensus_scrapr.R
@@ -8,43 +8,43 @@ library(readr)
 library(ggplot2)
 
 library(scales)
-install.packages("corrplot")
+#install.packages("corrplot")
 library(corrplot)
-#install.packages("orcutt")
+##install.packages("orcutt")
 #library(orcutt)
-#install.packages("glmnet")
+##install.packages("glmnet")
 library(glmnet)
-install.packages("Rtools")
+#install.packages("Rtools")
 #library(Rtools)
-#install.packages("sandwich")
+##install.packages("sandwich")
 library(sandwich)
 
-#install.packages("tseries")
+##install.packages("tseries")
 options(warn=-1)
 library(tseries)
-#install.packages("lmtest")
+##install.packages("lmtest")
 library(lmtest)
 
-#install.packages("stargazer")
+##install.packages("stargazer")
 library(stargazer)
 
-install.packages("DataCombine")
+#install.packages("DataCombine")
 library(DataCombine)
 
-install.packages("ivreg")
+#install.packages("ivreg")
 library(ivreg)
 
-#install.packages("forecast")
+##install.packages("forecast")
 library(forecast)
 
-install.packages("margins")
+#install.packages("margins")
 library("margins")
 
-install.packages("cancensus")
+#install.packages("cancensus")
 
 library(cancensus)
 
-install.packages("rjson")
+#install.packages("rjson")
 library(rjson)
 
 # set_cache_path("C:/Users/profs/OneDrive/Documents/anindya", install = TRUE)
@@ -259,31 +259,82 @@ write.csv(Oakdata25, "Oakdata25.csv")
 #Assuming that 2a has all the data required for the analysis.
 #Creating a for loop to get data for all CSDs in Ontario
 
-list_census_regions('CA16')
-regions <- list_census_regions("CA16")# Get all census regions
-csd_regions <- regions[regions$level == "CSD", ]  # Filter for Census Subdivisions (CSDs)
-print(csd_regions)
+### YEAR 2011 ###
+
+list_census_regions('CA11')
+regions_11 <- list_census_regions("CA11")# Get all census regions
+csd_regions_11 <- regions_11[regions_11$level == "CSD", ]  # Filter for Census Subdivisions (CSDs)
+print(csd_regions_11)
 
 # filter data for top 50 pop cities
-csd_regions <- csd_regions %>%
+csd_regions_11 <- csd_regions_11 %>%
   arrange(desc(pop)) %>%  # Sort by population in descending order
   head(5)  # Select the top 50
 
 # create a folder
-output_folder <- "census_data16"  # Change to your desired path
-if (!dir.exists(output_folder)) {
-  dir.create(output_folder, recursive = TRUE)
+output_folder_11 <- "census_data11"  # Change to your desired path
+if (!dir.exists(output_folder_11)) {
+  dir.create(output_folder_11, recursive = TRUE)
+}
+
+for (i in 1:nrow(csd_regions_11)) {
+  csd_id_11 <- csd_regions_11$region[i]  
+  csd_name_11 <- csd_regions_11$name[i]  
+  print(csd_name_11)
+  
+  data_11 <- get_census(
+    dataset = "CA11",
+    regions = list(CSD = csd_id_11),
+    vectors = c("v_CA11_454","v_CA11_463","v_CA11_466","v_CA11_469","v_CA11_472","v_CA11_475","v_CA11_2207","v_CA11_2209","v_CA11_2208", "v_CA11_408","v_CA11_409",
+                "v_CA11_410","v_CA11_411","v_CA11_417","v_CA11_479","v_CA11_480","v_CA11_481","v_CA11_482","v_CA11_483",
+                "v_CA11_2309","v_CA11_2312","v_CA11_2315","v_CA11_2318","v_CA11_2321",
+                "v_CA11_2324","v_CA11_2327","v_CA11_2330","v_CA11_2333","v_CA11_2336","v_CA11_2339",
+                "v_CA11_2342","v_CA11_2570","v_CA11_2540","v_CA11_5795","v_CA11_5798",
+                "v_CA11_5801","v_CA11_5804","v_CA11_5807","v_CA11_5810","v_CA11_5816","v_CA11_5819",
+                "v_CA11_5822","v_CA11_5825","v_CA11_5828",
+                "v_CA11_2427","v_CA11_2428","v_CA11_2429","v_CA11_2430","v_CA11_2431","v_CA11_2432","v_CA11_2433","v_CA11_2434","v_CA11_2435","v_CA11_2436",
+                "v_CA11_2437","v_CA11_2438","v_CA11_2439","v_CA11_2440","v_CA11_2441","v_CA11_2442",
+                "v_CA11_4893","v_CA11_4894","v_CA11_4895","v_CA11_4896","v_CA11_4891","v_CA11_4892",
+                "v_CA11_4897","v_CA11_4898","v_CA11_4898","v_CA11_4899","v_CA11_4900","v_CA11_4901",
+                "v_CA11_5099","v_CA11_5102","v_CA11_5108","v_CA11_5117","v_CA11_5120","v_CA11_5123",
+                "v_CA11_5603","v_CA11_5606","v_CA11_5609","v_CA11_5663","v_CA11_5669","v_CA11_5672","v_CA11_5675",
+                "v_CA11_5678","v_CA11_5681","v_CA11_5684","v_CA11_5687","v_CA11_5690",
+                "v_CA11_4002","v_CA11_4806","v_CA11_4014","v_CA11_4044","v_CA11_4266","v_CA11_4329","v_CA11_4611","v_CA11_4698",
+                "v_CA11_4743","v_CA11_4800","v_CA11_3408","v_CA11_3411","v_CA11_3435"), 
+    level = "DA"
+  )  
+  # Save CSV to the specified folder
+  write.csv(data_11, file = file.path(output_folder_11, paste0(csd_name_11, "_11.csv")))
 }
 
 
-for (i in 1:nrow(csd_regions)) {
-  csd_id <- csd_regions$region[i]  
-  csd_name <- csd_regions$name[i]  
-  print(csd_name)
+### YEAR 2016 ###
+
+
+list_census_regions('CA16')
+regions_16 <- list_census_regions("CA16")# Get all census regions
+csd_regions_16 <- regions_16[regions_16$level == "CSD", ]  # Filter for Census Subdivisions (CSDs)
+print(csd_regions_16)
+
+# filter data for top 50 pop cities
+csd_regions_16 <- csd_regions_16 %>%
+  arrange(desc(pop)) %>%  # Sort by population in descending order
+  head(5)  # Select the top 50
+
+# create a folder
+output_folder_16 <- "census_data16"  # Change to your desired path
+if (!dir.exists(output_folder_16)) {
+  dir.create(output_folder_16, recursive = TRUE)
+}
+
+for (i in 1:nrow(csd_regions_16)) {
+  csd_id_16 <- csd_regions_16$region[i]  
+  csd_name_16 <- csd_regions_16$name[i]  
+  print(csd_name_16)
   
-  data <- get_census(
+  data_16 <- get_census_16(
     dataset = "CA16",
-    regions = list(CSD = csd_id),
+    regions = list(CSD = csd_id_16),
     vectors = c("v_CA16_454","v_CA16_463","v_CA16_466","v_CA16_469","v_CA16_472","v_CA16_475","v_CA16_2207","v_CA16_2209","v_CA16_2208", "v_CA16_408","v_CA16_409",
                 "v_CA16_410","v_CA16_411","v_CA16_417","v_CA16_479","v_CA16_480","v_CA16_481","v_CA16_482","v_CA16_483",
                 "v_CA16_2309","v_CA16_2312","v_CA16_2315","v_CA16_2318","v_CA16_2321",
@@ -302,9 +353,82 @@ for (i in 1:nrow(csd_regions)) {
                 "v_CA16_4743","v_CA16_4800","v_CA16_3408","v_CA16_3411","v_CA16_3435"), 
     level = "DA"
   )
-  
   # Save CSV to the specified folder
-  write.csv(data, file = file.path(output_folder, paste0(csd_name, "_16.csv")))
+  write.csv(data_16, file = file.path(output_folder_16, paste0(csd_name_16, "_16.csv")))
 }
+
+### YEAR 2021 ###
+
+list_census_regions('CA21')
+regions_21 <- list_census_regions("CA21")# Get all census regions
+csd_regions_21 <- regions_21[regions_21$level == "CSD", ]  # Filter for Census Subdivisions (CSDs)
+print(csd_regions_21)
+
+# filter data for top 50 pop cities
+csd_regions_21 <- csd_regions_21 %>%
+  arrange(desc(pop)) %>%  # Sort by population in descending order
+  head(5)  # Select the top 50
+
+# create a folder
+output_folder_21 <- "census_data21"  # Change to your desired path
+if (!dir.exists(output_folder_21)) {
+  dir.create(output_folder_21, recursive = TRUE)
+}
+  
+for (i in 1:nrow(csd_regions_21)) {
+  csd_id_21 <- csd_regions_21$region[i]  
+  csd_name_21 <- csd_regions_21$name[i]  
+  print(csd_name_21)
+  
+  data_21 <- get_census(
+    dataset = "CA21",
+    regions = list(CSD = csd_id_21),
+    vectors = c("v_CA21_454","v_CA21_463","v_CA21_466","v_CA21_469","v_CA21_472","v_CA21_475","v_CA21_2207","v_CA21_2209","v_CA21_2208", "v_CA21_408","v_CA21_409",
+                "v_CA21_410","v_CA21_411","v_CA21_417","v_CA21_479","v_CA21_480","v_CA21_481","v_CA21_482","v_CA21_483",
+                "v_CA21_2309","v_CA21_2312","v_CA21_2315","v_CA21_2318","v_CA21_2321",
+                "v_CA21_2324","v_CA21_2327","v_CA21_2330","v_CA21_2333","v_CA21_2336","v_CA21_2339",
+                "v_CA21_2342","v_CA21_2570","v_CA21_2540","v_CA21_5795","v_CA21_5798",
+                "v_CA21_5801","v_CA21_5804","v_CA21_5807","v_CA21_5810","v_CA21_5816","v_CA21_5819",
+                "v_CA21_5822","v_CA21_5825","v_CA21_5828",
+                "v_CA21_2427","v_CA21_2428","v_CA21_2429","v_CA21_2430","v_CA21_2431","v_CA21_2432","v_CA21_2433","v_CA21_2434","v_CA21_2435","v_CA21_2436",
+                "v_CA21_2437","v_CA21_2438","v_CA21_2439","v_CA21_2440","v_CA21_2441","v_CA21_2442",
+                "v_CA21_4893","v_CA21_4894","v_CA21_4895","v_CA21_4896","v_CA21_4891","v_CA21_4892",
+                "v_CA21_4897","v_CA21_4898","v_CA21_4898","v_CA21_4899","v_CA21_4900","v_CA21_4901",
+                "v_CA21_5099","v_CA21_5102","v_CA21_5108","v_CA21_5117","v_CA21_5120","v_CA21_5123",
+                "v_CA21_5603","v_CA21_5606","v_CA21_5609","v_CA21_5663","v_CA21_5669","v_CA21_5672","v_CA21_5675",
+                "v_CA21_5678","v_CA21_5681","v_CA21_5684","v_CA21_5687","v_CA21_5690",
+                "v_CA21_4002","v_CA21_4806","v_CA21_4014","v_CA21_4044","v_CA21_4266","v_CA21_4329","v_CA21_4611","v_CA21_4698",
+                "v_CA21_4743","v_CA21_4800","v_CA21_3408","v_CA21_3411","v_CA21_3435"), 
+    level = "DA"
+  )  
+  # Save CSV to the specified folder
+  write.csv(data_21, file = file.path(output_folder_21, paste0(csd_name_21, "_21.csv")))
+}
+  
+
+# Define your vectors
+vectors <- c("v_CA16_454","v_CA16_463","v_CA16_466","v_CA16_469","v_CA16_472","v_CA16_475","v_CA16_2207","v_CA16_2209","v_CA16_2208", 
+             "v_CA16_408","v_CA16_409","v_CA16_410","v_CA16_411","v_CA16_417","v_CA16_479","v_CA16_480","v_CA16_481","v_CA16_482",
+             "v_CA16_483","v_CA16_2309","v_CA16_2312","v_CA16_2315","v_CA16_2318","v_CA16_2321","v_CA16_2324","v_CA16_2327",
+             "v_CA16_2330","v_CA16_2333","v_CA16_2336","v_CA16_2339","v_CA16_2342","v_CA16_2570","v_CA16_2540","v_CA16_5795",
+             "v_CA16_5798","v_CA16_5801","v_CA16_5804","v_CA16_5807","v_CA16_5810","v_CA16_5816","v_CA16_5819","v_CA16_5822",
+             "v_CA16_5825","v_CA16_5828","v_CA16_2427","v_CA16_2428","v_CA16_2429","v_CA16_2430","v_CA16_2431","v_CA16_2432",
+             "v_CA16_2433","v_CA16_2434","v_CA16_2435","v_CA16_2436","v_CA16_2437","v_CA16_2438","v_CA16_2439","v_CA16_2440",
+             "v_CA16_2441","v_CA16_2442","v_CA16_4893","v_CA16_4894","v_CA16_4895","v_CA16_4896","v_CA16_4891","v_CA16_4892",
+             "v_CA16_4897","v_CA16_4898","v_CA16_4898","v_CA16_4899","v_CA16_4900","v_CA16_4901","v_CA16_5099","v_CA16_5102",
+             "v_CA16_5108","v_CA16_5117","v_CA16_5120","v_CA16_5123","v_CA16_5603","v_CA16_5606","v_CA16_5609","v_CA16_5663",
+             "v_CA16_5669","v_CA16_5672","v_CA16_5675","v_CA16_5678","v_CA16_5681","v_CA16_5684","v_CA16_5687","v_CA16_5690",
+             "v_CA16_4002","v_CA16_4806","v_CA16_4014","v_CA16_4044","v_CA16_4266","v_CA16_4329","v_CA16_4611","v_CA16_4698",
+             "v_CA16_4743","v_CA16_4800","v_CA16_3408","v_CA16_3411","v_CA16_3435")
+
+# Get all available variables
+variables <- list_census_vectors("CA16")
+
+matched_results <- variables[variables$vector %in% vectors, , drop = FALSE]
+print(matched_results)
+
+# Save the matched results as a CSV file
+write.csv(matched_results, "matched_variables.csv", row.names = FALSE)
+
 
 print("Done!")
